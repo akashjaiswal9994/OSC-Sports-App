1cd755ba83664c53bbf43a8f3f69e1f6cb0fe6a6
akashjaiswal9994 <jaiswalakash99.94@gmail.com>
2022-07-11 12:15:04 +0530

    adding models and database

--- BackEnd/sport/models.py
+++ BackEnd/sport/models.py
@@ -2,5 +2,30 @@ from django.db import models
 
 # Create your models here.
 
+class Player(models.Model):
+    SPORT_TYPE=[
+        ('None','NONE' ),
+        ('Chess','CHESS' ),
+        ('Basket Ball','BASKET BALL' ),
+        ('FootBall','FOOTBALL' ),
+        ('Cricket','CRICKET' ),
+        ('Volley Ball ','VOLLEY BALL' ),
+        ('Floor Ball','FLOOR BALL' ),
+        ('Table Tennis','TABLE TENNIS'),
+        ('MARTIAL ART','MARTIAL ART' ),
+        ('Kayaking','Kayaking' ),
+        (' Bobsleighing',' Bobsleighing' ),
+        (' Canoeing',' Canoeing' ),
+        ('Skiing','Skiing' ),
+        ('Surfing','Surfing' ), 
+        ('Snorkeling','Snorkeling' ), 
+        ('Olympic Swimming','Olympic Swimming' )
+    ]
+    player_id=models.AutoField(primary_key=True)
+    first_name=models.CharField(max_length=200)
+    last_name=models.CharField(max_length=200)
+    date_of_birth=models.DateField()
+    email=models.EmailField()
+    Sport=models.CharField(max_length=50,choices=SPORT_TYPE,default='None')
 
 
--- BackEnd/sportApp/settings.py
+++ BackEnd/sportApp/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'sportApp.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'speedways',
+        'USER':'postgres',
+        'PASSWORD':'123456789',
+        'HOST':'localhost'
     }
+
 }
 
 
